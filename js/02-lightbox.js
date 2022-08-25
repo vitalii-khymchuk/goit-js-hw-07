136b80c5d66cfe7aae6e7cc285677672fe367d2f
vitalii-khymchuk <vitalii.workbox001@gmail.com>
2022-08-25 22:35:17 +0300

    task-02 done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,28 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const refs = {
+  gallery: document.querySelector(".gallery"),
+  galleryItem: document.querySelector(".gallery__item"),
+  galleryImage: document.querySelector(".gallery__image"),
+};
+
+addMarkup(galleryItems);
+
+function addMarkup(items) {
+  refs.gallery.innerHTML = createMarkup(items);
+}
+
+function createMarkup(items) {
+  return items.map((element) => createElementMarkup(element)).join("");
+}
+
+function createElementMarkup(element) {
+  return `<a class="gallery__item" href="${element.original}">
+  <img class="gallery__image" src="${element.preview}" alt="${element.description}"/>
+</a>`;
+}
+
+var lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
